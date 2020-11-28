184db7e521719e1387dd5fc655154791d31454cb
Mantas <j.mantelijo@gmail.com>
2020-11-28 14:13:32 +0200

    Dropzone

--- src/components/dropzone/Dropzone.vue
+++ src/components/dropzone/Dropzone.vue
@@ -1,19 +1,25 @@
 <template>
-    <div :class="['dropzone p-5 rounded', {'active':active}]" ref="dropzone">
-        <input type="file" multiple @change="handleFileInput" />
-        <div class="d-flex flex-column align-items-center">
-            <slot :items="items" :active="active">
-                <div v-if="!active">
-                    <div class="cursor-pointer">
-                        Drop files or <u>click here</u> to browse
+    <div :class="['dropzone p-5 rounded', {'active':active}]" ref="dropzone" @click="browseFilesOnClick?clickFile():null;">
+        <input type="file" multiple @change="handleFileInput" ref="fileInput" />
+        <div class="dropzone-content">
+            <slot :items="files" :active="active" :remove-file="removeFile" :browse="clickFile">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <div v-if="!active">
+                        <p class="cursor-pointer m-0">
+                            Drag and Drop files or <u @click="clickFile">click here</u> to browse
+                        </p>
+                        <div v-for="(file, key) in files" class="my-1">
+                            <b>{{file.name}}</b>
+                            &nbsp;
+                            <span>{{formatFileSize(file.size)}}</span>
+                            &nbsp;
+
+                            <span @click="removeFile(key)" class="hover-underline cursor-pointer">(remove)</span>
+                        </div>
                     </div>
-                    <div v-for="file in items">
-                        <b>{{file.name}}</b>
-                        <span>{{formatFileSize(file.size)}}</span>
+                    <div v-else>
+                        Drop it like it's hot!
                     </div>
-                </div>
-                <div v-else>
-                    Drop it like it's hot!
                 </div>
             </slot>
         </div>
@@ -26,14 +32,27 @@ import {formatFileSize} from "@/helpers/helpers";
 export default {
     name: "Dropzone",
 
+    props:{
+        browseFilesOnClick:{
+            type:Boolean,
+            required:false,
+            default:false,
+            description: 'If set to true browse files window will be opened when clicking on dropzone.'
+        }
+    },
+
     data(){
         return {
-            items:[],
+            // Array of added files
+            files:[],
+
+            // Whether currently something is being dragged over dropzone.
             active:false,
         }
     },
 
     methods:{
+        // Handle input[type=file]
         handleFileInput(e){
             let files = e.target.files;
 
@@ -42,20 +61,29 @@ export default {
             }
         },
 
+        // Drag enter events
         enter(e){
+            e.preventDefault();
             if(this.active === false){
                 this.active = true;
             }
-            console.log("entering");
         },
 
+        // Drag leave events
         leave(e){
+            e.preventDefault();
+
+            // Prevent child event triggers
+            if(e.target !== this.$refs.dropzone){
+                return;
+            }
+
             if(this.active === true){
                 this.active = false;
             }
-            console.log("leaving");
         },
 
+        // Handle dropped files
         handleDrop(e){
             e.preventDefault();
 
@@ -66,11 +94,23 @@ export default {
             }
         },
 
+        // Push single file to data property
         handleFile(file){
-            this.items.push(file);
+            this.files.push(file);
         },
 
+        // Helper to format file size
         formatFileSize,
+
+        // Emulate input click
+        clickFile(){
+            this.$refs.fileInput.click();
+        },
+
+        // Remove file from current items list based on provided index
+        removeFile(index){
+            this.files = this.files.filter(item=>this.files.indexOf(item) !== index);
+        },
     },
 
     mounted(){
@@ -82,7 +122,7 @@ export default {
                 dropzone.addEventListener(eventName, this.enter);
             });
             // Leave events
-            ['dragleave', 'drop'].forEach(eventName => {
+            ['drop', 'dragleave'].forEach(eventName => {
                 dropzone.addEventListener(eventName, this.leave);
             });
 
