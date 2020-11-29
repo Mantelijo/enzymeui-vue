729639565386b519270f0937dbcde9f706f658d2
Mantas <j.mantelijo@gmail.com>
2020-11-29 14:38:05 +0200

    Small changes

--- src/plugins/loader.js
+++ src/plugins/loader.js
@@ -41,9 +41,6 @@ import Dropzone from "../components/dropzone/Dropzone";
 
 // Charts
 import ChartsComponent from '../components/charts/Chart';
-import {generateMdTable} from "./../helpers/propsTableGenerator";
-
-console.log(generateMdTable(Dropzone));
 
 export default {
     install(Vue){
